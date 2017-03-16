dc6a17eea005464d348c4c0ea381e78f0724c11c
Piermarco Zerbini <p.zerbini@doveconviene.com>
2017-03-16 11:19:43 +0100

    feat: adds database sync per table

---
+++ mysql_sync_database.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+if ! type mapfile > /dev/null 2>&1 ; then
+  echo "This script need bash 4.x exiting"
+	exit 2
+fi
+set -e
+
+PARSED_OPTIONS=$(getopt -n "$0" -o h --long "source:,target:,db:,user:,password:,tables:"  -- "$@")
+
+while true;
+do
+  case "$1" in
+    --source )
+	  SOURCE_MYSQL_HOST=$2
+      shift 2;;
+    --target )
+  	  TARGET_MYSQL_HOST=$2
+      shift 2;;
+    --db )
+      DB=$2
+      shift 2;;
+    --user )
+	  DB_USER=$2
+	  shift 2;;
+	--password )
+	  DB_PASS=$2
+	  shift 2;;
+	--tables )
+	  shift
+	  TABLES=$@
+	  break;;
+    -- )
+      shift
+      break;;
+	* ) break ;;
+  esac
+done
+
+echo "### Inputs ###
+SOURCE_MYSQL_HOST = $SOURCE_MYSQL_HOST
+TARGET_MYSQL_HOST = $TARGET_MYSQL_HOST
+DB = $DB
+DB_USER = $DB_USER
+DB_PASS = $DB_PASS
+TABLES = $TABLES
+"
+DUMP_OPTS="--ignore-table=mysql.event --hex-blob --add-drop-table --single-transaction --skip-add-locks"
+
+# CHECK SOURCE_MYSQL_HOST if database exists
+echo "CHECK connection and grants to ${SOURCE_MYSQL_HOST}"
+mysql --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
+
+# CHECK TARGET_MYSQL_HOST if database exists
+echo "CHECK connection and grants to ${TARGET_MYSQL_HOST}"
+mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
+
+exit 0
+
+echo "Starting migration"
+for TABLE in ${ALLTABLES[@]}
+do
+    echo "Migrating table $TABLE..."
+	mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | mysql --host=${TARGET_MYSQL_HOST} --user=dbsync --password=${dbsync} $DB
+
+done
+echo "done."
