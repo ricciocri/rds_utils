5801525173fae3b6f7966ab31436193ac11b1d73
scakkia <scakkia@users.noreply.github.com>
2023-06-28 11:42:49 +0200

    test

--- mysql_sync_database_docker.sh
+++ mysql_sync_database_docker.sh
@@ -51,19 +51,20 @@ DOCKER_CMD="docker run -i ${USE_TTY} --rm mysql:8.0"
 DUMP_OPTS="--ignore-table=mysql.event --hex-blob --add-drop-table --single-transaction --skip-add-locks --no-tablespaces"
 
 # CHECK SOURCE_MYSQL_HOST if database exists
-echo "CHECK connection and grants to ${SOURCE_MYSQL_HOST}"
-mysql --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
+echo "CHECK connection and grants on SOURCE ${SOURCE_MYSQL_HOST}"
+${DOCKER_CMD} mysql --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
 
 # CHECK TARGET_MYSQL_HOST if database exists
-echo "CHECK connection and grants to ${TARGET_MYSQL_HOST}"
-mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
+echo "CHECK connection and grants toon TARGET ${TARGET_MYSQL_HOST}"
+${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
 
 
 echo "Starting migration"
 for TABLE in ${TABLES}
 do
 	echo "Migrating $TABLE"
-	${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command="SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;" $DB
+#	${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command="SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;" $DB
+echo	${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command="SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;" $DB
 
 done
 echo "done."
