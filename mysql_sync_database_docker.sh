f9363779e22e4e15f552e27f04ceeaf29caad077
scakkia <scakkia@users.noreply.github.com>
2023-06-28 11:46:46 +0200

    test

--- mysql_sync_database_docker.sh
+++ mysql_sync_database_docker.sh
@@ -51,11 +51,11 @@ DOCKER_CMD="docker run -i ${USE_TTY} --rm mysql:8.0"
 DUMP_OPTS="--ignore-table=mysql.event --hex-blob --add-drop-table --single-transaction --skip-add-locks --no-tablespaces"
 
 # CHECK SOURCE_MYSQL_HOST if database exists
-echo "CHECK connection and grants on SOURCE ${SOURCE_MYSQL_HOST}"
+echo "CHECK connection and grants on SOURCE_MYSQL_HOST ${SOURCE_MYSQL_HOST} and DB ${DB}"
 ${DOCKER_CMD} mysql --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
 
 # CHECK TARGET_MYSQL_HOST if database exists
-echo "CHECK connection and grants toon TARGET ${TARGET_MYSQL_HOST}"
+echo "CHECK connection and grants toon TARGET_MYSQL_HOST ${TARGET_MYSQL_HOST} and DB ${DB}"
 ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} -e 'show tables;' ${DB}
 
 
@@ -64,7 +64,7 @@ for TABLE in ${TABLES}
 do
 	echo "Migrating $TABLE"
 #	${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command="SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;" $DB
-echo	${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command="SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;" $DB
+echo	"${DOCKER_CMD} mysqldump --host=${SOURCE_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} ${DUMP_OPTS} ${DB} ${TABLE} | ${DOCKER_CMD} mysql --host=${TARGET_MYSQL_HOST} --user=${DB_USER} --password=${DB_PASS} --init-command=\"SET SESSION FOREIGN_KEY_CHECKS=0; SET SESSION UNIQUE_CHECKS=0;\" ${DB}"
 
 done
 echo "done."
